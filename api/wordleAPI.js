e868b3527caf49b04ea36565ab501770aa0cba44
Shan Jiang <shanjiang2005@gmail.com>
2025-03-19 23:27:38 -0400

    i think wordle should work now

--- api/wordleAPI.js
+++ api/wordleAPI.js
@@ -1,15 +1,18 @@
 export default async function handler(req, res) {
-    const date = new Date();
-    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`
-    const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`
-    const wordleApiUrl = `https://www.nytimes.com/svc/wordle/v2/${date.getFullYear()}-${month}-${day}.json`;
-  
-    try {
-      const response = await fetch(wordleApiUrl);
-      const data = await response.json();
-      res.status(200).json(data); // Send the data to your frontend
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch Wordle API' });
-    }
+  const { date } = req.query; // Extract the date from the query parameter
+  // Validate the provided date (optional)
+  if (!date) {
+    return res.status(400).json({ error: 'Date is required' });
   }
+
+  const wordleApiUrl = `https://www.nytimes.com/svc/wordle/v2/${date}.json`;
+  try {
+    const response = await fetch(wordleApiUrl);
+    const data = await response.json();
+    res.status(200).json(data); // Send the data to your frontend
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch Wordle API' });
+  }
+}
+
   
--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,7 +1,13 @@
-fetch('/api/wordleAPI')
+let solution;
+const date = new Date();
+const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+const dateString = `${date.getFullYear()}-${month}-${day}`;
+
+fetch(`/api/wordleAPI?date=${dateString}`)
   .then(response => response.json())
   .then(data => {
-    const solution = data.solution;
+    solution = data.solution;
     console.log(`solution for today is ${solution}`);
     document.getElementById("wordle").innerHTML = "<wordle-for-good " +
     `word=\"${solution.charCodeAt(0)}-${solution.charCodeAt(1)}-${solution.charCodeAt(2)}-${solution.charCodeAt(3)}-${solution.charCodeAt(4)}\" ` + 
@@ -17,8 +23,14 @@ fetch('/api/wordleAPI')
     "key-text-color=\"#ffffff\" " +
     "key-text-size=\"1.3rem\" " +
     "height=\"700px\" " +
-    "title=\"Wordle For Good\"></wordle-for-good>";
+    "title=\"Wordle\" " +
+    "success-selector=\"#win\" " +
+    "failure-selector=\"#lose\" " +    
+    "></wordle-for-good>";
 
+    // update win and lose
+    document.getElementById("wintext").innerHTML = `The word is: <strong>${solution}</strong>`;
+    document.getElementById("losetext").innerHTML = `The word is: <strong>${solution}</strong>`;
   })
   .catch(error => console.error('Error: ', error));
 
